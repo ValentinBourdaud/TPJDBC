aa70c7453587d780e8e16933b84002fcc0eab1d5
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-24 17:13:51 +0200

    Test ajouter pizza service passant

--- src/test/java/org/diginamic/pizzeria/TestAjouterPizzaService.java
+++ src/test/java/org/diginamic/pizzeria/TestAjouterPizzaService.java
@@ -1,5 +1,32 @@
 package org.diginamic.pizzeria;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+
+import java.util.Scanner;
+
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.pizzeria.dao.PizzaMemDao;
+import fr.pizzeria.model.Pizza;
+import fr.pizzeria.services.AjouterPizzaService;
+
 public class TestAjouterPizzaService {
 
+	@Rule
+	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+	
+	@org.junit.Test
+	public void testExecuteUC(){
+		
+		AjouterPizzaService aps = new AjouterPizzaService();
+		PizzaMemDao pizza = new PizzaMemDao();
+		
+		systemInMock.provideLines("CAR", "LaCarbonara", "12,00");
+		Pizza p = new Pizza("CAR", "LaCarbonara", 12.00);
+		aps.executeUC(new Scanner(System.in), pizza);
+		assertTrue(pizza.findAllPizzas().contains(p));
+		
+	}
 }
