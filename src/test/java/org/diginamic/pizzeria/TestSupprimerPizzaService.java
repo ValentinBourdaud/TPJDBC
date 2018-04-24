434c0316857672781ed59070b98370e66ce5818b
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-24 17:33:13 +0200

    Test supprimer pizza service passant

--- src/test/java/org/diginamic/pizzeria/TestSupprimerPizzaService.java
+++ src/test/java/org/diginamic/pizzeria/TestSupprimerPizzaService.java
@@ -1,5 +1,30 @@
 package org.diginamic.pizzeria;
 
-public class TestSupprimerPizzaService {
+import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
+import java.util.Scanner;
+
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.pizzeria.dao.PizzaMemDao;
+import fr.pizzeria.services.SupprimerPizzaService;
+
+public class TestSupprimerPizzaService {
+	
+	@Rule
+	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+	
+	@org.junit.Test
+	public void testExecuteUC(){
+		
+		SupprimerPizzaService aps = new SupprimerPizzaService();
+		PizzaMemDao pizza = new PizzaMemDao();
+		
+		systemInMock.provideLines("FRO");
+		aps.executeUC(new Scanner(System.in), pizza);
+		assertTrue(pizza.findAllPizzas().size()==7);
+
+	}
 }
