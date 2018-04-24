459ff4b95f231f7bf205f0445ccd2fc4afecda32
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-24 17:27:31 +0200

    Test modifier pizza service passant

--- src/test/java/org/diginamic/pizzeria/TestAuRevoirService.java
+++ src/test/java/org/diginamic/pizzeria/TestAuRevoirService.java
@@ -1,5 +1,26 @@
 package org.diginamic.pizzeria;
 
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.pizzeria.dao.PizzaMemDao;
+import fr.pizzeria.services.AjouterPizzaService;
+
 public class TestAuRevoirService {
 
+	@Rule
+	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+	
+	@org.junit.Test
+	public void testExecuteUC(){
+		AjouterPizzaService aps = new AjouterPizzaService();
+		PizzaMemDao pizza = new PizzaMemDao();
+		
+		systemInMock.provideLines("Au revoir");
+		
+
+		
+	}
 }
--- src/test/java/org/diginamic/pizzeria/TestModifierPizzaService.java
+++ src/test/java/org/diginamic/pizzeria/TestModifierPizzaService.java
@@ -1,5 +1,32 @@
 package org.diginamic.pizzeria;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+
+import java.util.Scanner;
+
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.pizzeria.dao.PizzaMemDao;
+import fr.pizzeria.model.Pizza;
+import fr.pizzeria.services.ModifierPizzaService;
+
 public class TestModifierPizzaService {
 
+	@Rule
+	public TextFromStandardInputStream systemInMock = emptyStandardInputStream();
+	
+	@org.junit.Test
+	public void testExecuteUC(){
+		
+		ModifierPizzaService aps = new ModifierPizzaService();
+		PizzaMemDao pizza = new PizzaMemDao();
+		
+		systemInMock.provideLines("FRO", "CAR", "LaCarbonara", "12,00");
+		Pizza p = new Pizza("CAR", "LaCarbonara", 12.00);
+		aps.executeUC(new Scanner(System.in), pizza);
+    	assertTrue(pizza.findAllPizzas().contains(p));
 }
+}
+
